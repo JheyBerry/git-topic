f33c437670d4950a499115e90a59b80089047187
Jheney Vilvok <57020774+JheyBerry@users.noreply.github.com>
2021-02-07 15:45:09 -0300

    build: adding auth0 config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={process.env.REACT_APP_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root'),
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
