2a40edc6153baf6bcc81ff277e31d429f1165f51
Jheney Vilvok <57020774+JheyBerry@users.noreply.github.com>
2021-02-07 22:00:08 -0300

    feat: adding footer and navbar

---
+++ src/components/nav-bar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import MainNav from './main-nav'
+import AuthNav from './auth-nav'
+
+const NavBar = () => (
+  <div className="nav-container mb-3">
+    <nav className="navbar navbar-expand-md navbar-light bg-light">
+      <div className="container">
+        <div className="navbar-brand logo" />
+        <MainNav />
+        <AuthNav />
+      </div>
+    </nav>
+  </div>
+)
+
+export default NavBar
