0ddf66ffcbe2cdea5b0b8540fed82346c940b3e8
Jheney Vilvok <57020774+JheyBerry@users.noreply.github.com>
2021-02-07 22:02:45 -0300

    feat: adding pages

---
+++ src/views/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home= () => {
+
+  return (
+      <div className="row align-items-center profile-header">
+        <h1>welcome miau miau</h1>
+       </div>
+  );
+};
+
+export default Home;
