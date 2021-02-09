4c2ec62588da707f5ef02a2071ae9c5f0a8ef94f
Jheney Vilvok <57020774+JheyBerry@users.noreply.github.com>
2021-02-08 22:19:52 -0300

    chore: cleaning useless files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-import App from './app'
+import App from './App'
 import Auth0ProviderWithHistory from './auth/auth0-provider-with-history'
 
 import './index.css'
