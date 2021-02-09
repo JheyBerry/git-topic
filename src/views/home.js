e35fc63461d7c35dbcdb763e0aa3a9791069de71
Jheney Vilvok <57020774+JheyBerry@users.noreply.github.com>
2021-02-09 12:48:18 -0300

    feat: adding home info

--- src/views/home.js
+++ src/views/home.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React from "react"
+import drawing from "./../assets/drawing.png"
 
 const Home= () => {
 
   return (
-      <div className="row align-items-center profile-header">
-        <h1>welcome miau miau</h1>
-       </div>
+      <div className="align-items-center row profile-header">
+        <div style={{width:"50%"}}>
+          <h1>Welcome to Git Topics</h1>
+          <h2>The easiest way to tag and find your repositories.</h2>
+        </div>
+        <div>
+          <img src={drawing} style={{width:"40%"}}></img>
+        </div>
+      </div>
   );
 };
 
