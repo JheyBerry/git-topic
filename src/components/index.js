94fe5fc3ca3c627022dcb5422178064506e146f2
Jheney Vilvok <57020774+JheyBerry@users.noreply.github.com>
2021-02-08 22:24:47 -0300

    feat: adding topic list

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Footer from './footer'
 import Loading from './loading'
 import NavBar from './nav-bar'
 import Card from './card'
+import RepoCard from './repo-card'
 
-export { Footer, Loading, NavBar, Card }
+export { Footer, Loading, NavBar, Card, RepoCard }
