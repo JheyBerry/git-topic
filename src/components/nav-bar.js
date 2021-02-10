33b0578d721fabfd1133aaa784ee6dc39c26162d
Jheney Vilvok <57020774+JheyBerry@users.noreply.github.com>
2021-02-10 08:06:17 -0300

    feat: deleting topics

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -6,7 +6,7 @@ import AuthNav from './auth-nav'
 const NavBar = () => (
   <div className="nav-container mb-3">
     <nav className="navbar navbar-expand-md navbar-light bg-light">
-      <div className="container">
+      <div className="container-fluid" style={{padding:"0 10rem"}}>
         <div className="navbar-brand logo" />
         <MainNav />
         <AuthNav />
--- src/components/repo-card.js
+++ src/components/repo-card.js
@@ -7,6 +7,21 @@ const RepoCard = ({item}) => {
   const [showTopics, setShowTopics] = useState(false)
   const [topicsHTML, setTopics] = useState()
 
+  const deleteTopic = async (topic) => {
+    let config = {
+      headers: {
+        Accept: "application/vnd.github.mercy-preview+json"
+      }
+    }
+
+    await axios.put(`https://api.github.com/repos/${user.nickname}/${item.name}/topics`, {names: []}, config)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
   
   const getTopics = async () => {
     let config = {
@@ -14,22 +29,36 @@ const RepoCard = ({item}) => {
         Accept: "application/vnd.github.mercy-preview+json"
       }
     }
-
+    
     try {
       const response = await axios(`https://api.github.com/repos/${user.nickname}/${item.name}/topics`, config)
       console.log(response)
 
       const topics = response.data.names
-      console.log(topics)
+      
+      const noTopic = <p style={{padding:"2rem 7rem"}}>Any topic here yet</p>
 
-      const topicsList = topics.map((item, index) => {
-        return <span className="badge bg-warning text-dark" key={index}>{item}</span>
-          
+
+      const topicsButton = topics.map((item, index) => {
+        return (
+          <button className="badge btn-dark btn-lg"  key={index} style={{margin:"0.5rem"}}>
+            {item}
+            <a type="button" className="btn btn-warning btn-sm" style={{margin:"0.2rem"}}>E</a>
+            <a 
+              type="button" 
+              className="btn btn-danger btn-sm" 
+              style={{margin:"0.2rem"}}
+              onClick={() => {
+                deleteTopic(item)
+              }}>x</a>
+          </button>
+        ) 
       })
+      
+      const topicsList = topics.length === 0 ? noTopic : topicsButton
 
       setTopics(
         <ul>
-
           {topicsList}
         </ul>
       )
@@ -41,18 +70,26 @@ const RepoCard = ({item}) => {
   const topics = showTopics ? topicsHTML : <></>
     
   return (
-    <div className="card" key={item.id}>
-      <div className="card-body">
+    <div className="card" key={item.id} style={{display : 'inline-block', width:"100%"}}>
+      <div className="card-body" style={{float:"left", width:"50%"}}>
         <h5 className="card-title">{item.name}</h5>
         <h6 className="card-subtitle mb-2 text-muted">{item.id}</h6>
         <p className="card-text">{item.description}</p>
         <a href= {item.svn_url} target="_blank" className="card-link">{item.svn_url}</a>
       </div>
-      <button  type="button" className="btn btn-primary" onClick={() => {
-        setShowTopics(true)
-        getTopics()
-      }}>See Topics</button>
-      {topics}
+      <div style={{marginLeft:"50%", width:"50%", height:"100%"}}>
+      <button 
+        type="button" 
+        style={{margin:"0.5rem"}}
+        className="btn btn-danger" 
+        onClick={() => {
+          setShowTopics(true)
+          getTopics()
+        }}> See Topics </button>
+      <ul>
+        {topics}
+      </ul>  
+      </div>
     </div> 
   )
 }
