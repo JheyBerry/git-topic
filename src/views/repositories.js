9585744a452638c0f5d381d263a89f502830bdee
Jheney Vilvok <jheyvilvok@gmail.com>
2021-02-17 15:59:05 -0300

    Revert "chore: updating home"
    This reverts commit a4a690a4e7310281d32d89e6bc1c24413c835f40.

--- src/views/repositories.js
+++ src/views/repositories.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import Carousel from 'react-multi-carousel'
+import Carousel from 'react-elastic-carousel'
 import { useAuth0 } from "@auth0/auth0-react"
 import axios from 'axios'
 import { RepoCard } from '../components'
@@ -23,24 +23,6 @@ const MyRepos = () => {
     search()
   }, [])
 
-  const responsive = [
-    {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 7,
-      paritialVisibilityGutter: 60
-    },
-    {
-      breakpoint: { max: 1024, min: 464 },
-      items: 5,
-      paritialVisibilityGutter: 50
-    },
-    {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      paritialVisibilityGutter: 30
-    }
-  ]
-
   const listRepos = repos.length !== 0 ? (
     repos.map((item) => {
       return <RepoCard key={item.id} item={item}/>
@@ -48,22 +30,11 @@ const MyRepos = () => {
   ) : (
     <p>Any Repository</p>
   )
-  
-  const carousel = () => {
-    return (
-      <Carousel
-        ssr
-        partialVisbile
-        itemClass="image-item"
-        responsive={responsive}
-      >
-        {listRepos}
-      </Carousel>
-    );
-  };
 
   return (
-    carousel()
+    <Carousel>
+      {listRepos}
+    </Carousel>
   )
 }
 
